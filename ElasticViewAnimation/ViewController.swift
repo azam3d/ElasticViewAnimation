0a4449d2031e4c863769047334fa1b81dd03b80c
Muhammad Azam Bin Baderi <m.azam@Azams-MacBook.local>
2017-03-15 16:12:48 +0800

    Add bounce animation whenwe release the finger.

--- ElasticViewAnimation/ViewController.swift
+++ ElasticViewAnimation/ViewController.swift
@@ -17,6 +17,12 @@ class ViewController: UIViewController {
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
+    private var animating = false {
+        didSet {
+            view.isUserInteractionEnabled = !animating
+            displayLink.isPaused = !animating
+        }
+    }
     private let l3ControlPointView = UIView()
     private let l2ControlPointView = UIView()
     private let l1ControlPointView = UIView()
@@ -25,25 +31,27 @@ class ViewController: UIViewController {
     private let r2ControlPointView = UIView()
     private let r3ControlPointView = UIView()
     
+    private var displayLink: CADisplayLink!
+    
     // MARK: -
     override func loadView() {
         super.loadView()
         
-        l3ControlPointView.frame = CGRect(x: 0.0, y: 0.0, width: 3.0, height: 3.0)
-        l2ControlPointView.frame = CGRect(x: 0.0, y: 0.0, width: 3.0, height: 3.0)
-        l1ControlPointView.frame = CGRect(x: 0.0, y: 0.0, width: 3.0, height: 3.0)
-        cControlPointView.frame = CGRect(x: 0.0, y: 0.0, width: 3.0, height: 3.0)
-        r1ControlPointView.frame = CGRect(x: 0.0, y: 0.0, width: 3.0, height: 3.0)
-        r2ControlPointView.frame = CGRect(x: 0.0, y: 0.0, width: 3.0, height: 3.0)
-        r3ControlPointView.frame = CGRect(x: 0.0, y: 0.0, width: 3.0, height: 3.0)
-        
-        l3ControlPointView.backgroundColor = .red
-        l2ControlPointView.backgroundColor = .red
-        l1ControlPointView.backgroundColor = .red
-        cControlPointView.backgroundColor = .red
-        r1ControlPointView.backgroundColor = .red
-        r2ControlPointView.backgroundColor = .red
-        r3ControlPointView.backgroundColor = .red
+//        l3ControlPointView.frame = CGRect(x: 0.0, y: 0.0, width: 3.0, height: 3.0)
+//        l2ControlPointView.frame = CGRect(x: 0.0, y: 0.0, width: 3.0, height: 3.0)
+//        l1ControlPointView.frame = CGRect(x: 0.0, y: 0.0, width: 3.0, height: 3.0)
+//        cControlPointView.frame = CGRect(x: 0.0, y: 0.0, width: 3.0, height: 3.0)
+//        r1ControlPointView.frame = CGRect(x: 0.0, y: 0.0, width: 3.0, height: 3.0)
+//        r2ControlPointView.frame = CGRect(x: 0.0, y: 0.0, width: 3.0, height: 3.0)
+//        r3ControlPointView.frame = CGRect(x: 0.0, y: 0.0, width: 3.0, height: 3.0)
+//        
+//        l3ControlPointView.backgroundColor = .red
+//        l2ControlPointView.backgroundColor = .red
+//        l1ControlPointView.backgroundColor = .red
+//        cControlPointView.backgroundColor = .red
+//        r1ControlPointView.backgroundColor = .red
+//        r2ControlPointView.backgroundColor = .red
+//        r3ControlPointView.backgroundColor = .red
         
         view.addSubview(l3ControlPointView)
         view.addSubview(l2ControlPointView)
@@ -62,6 +70,10 @@ class ViewController: UIViewController {
         
         layoutControlPoints(baseHeight: minimalHeight, waveHeight: 0.0, locationX: view.bounds.width / 2.0)
         updateShapeLayer()
+        
+        displayLink = CADisplayLink(target: self, selector: #selector(ViewController.updateShapeLayer))
+        displayLink.add(to: RunLoop.main, forMode: RunLoopMode.defaultRunLoopMode)
+        displayLink.isPaused = true
     }
     
     // MARK: -
@@ -69,6 +81,20 @@ class ViewController: UIViewController {
     
     @objc fileprivate func panGestureDidMove(gesture: UIPanGestureRecognizer) {
         if gesture.state == .ended || gesture.state == .failed || gesture.state == .cancelled {
+            let centerY = minimalHeight
+            
+            animating = true
+            UIView.animate(withDuration: 0.9, delay: 0.0, usingSpringWithDamping: 0.57, initialSpringVelocity: 0.0, options: [], animations: { () -> Void in
+                self.l3ControlPointView.center.y = centerY
+                self.l2ControlPointView.center.y = centerY
+                self.l1ControlPointView.center.y = centerY
+                self.cControlPointView.center.y = centerY
+                self.r1ControlPointView.center.y = centerY
+                self.r2ControlPointView.center.y = centerY
+                self.r3ControlPointView.center.y = centerY
+            }, completion: { _ in
+                self.animating = false
+            })
         } else {
             let additionalHeight = max(gesture.translation(in: view).y, 0)
             let waveHeight = min(additionalHeight * 0.6, maxWaveHeight)
@@ -82,7 +108,7 @@ class ViewController: UIViewController {
     
     fileprivate func layoutControlPoints(baseHeight: CGFloat, waveHeight: CGFloat, locationX: CGFloat) {
         let width = view.bounds.width
-        let minLeftX = min((locationX - width / 2.0) * 0.28, 0.0)
+        let minLeftX = min((locationX - width / 2.0) * 0.28, 0.0) // this value can go less than zero
         let maxRightX = max(width + (locationX - width / 2.0) * 0.28, width)
         
         let leftPartWidth = locationX - minLeftX
@@ -106,18 +132,21 @@ class ViewController: UIViewController {
         let bezierPath = UIBezierPath()
         
         bezierPath.move(to: CGPoint(x: 0.0, y: 0.0))
-        bezierPath.addLine(to: CGPoint(x: 0.0, y: l3ControlPointView.dg_center(usePresentationLayerIfPossible: false).y))
-        bezierPath.addCurve(to: l1ControlPointView.dg_center(usePresentationLayerIfPossible: false),
-                            controlPoint1: l3ControlPointView.dg_center(usePresentationLayerIfPossible: false),
-                            controlPoint2: l2ControlPointView.dg_center(usePresentationLayerIfPossible: false))
-        bezierPath.addCurve(to: r1ControlPointView.dg_center(usePresentationLayerIfPossible: false),
-                            controlPoint1: cControlPointView.dg_center(usePresentationLayerIfPossible: false),
-                            controlPoint2: r1ControlPointView.dg_center(usePresentationLayerIfPossible: false))
-        bezierPath.addCurve(to: r3ControlPointView.dg_center(usePresentationLayerIfPossible: false),
-                            controlPoint1: r1ControlPointView.dg_center(usePresentationLayerIfPossible: false),
-                            controlPoint2: r2ControlPointView.dg_center(usePresentationLayerIfPossible: false))
-        bezierPath.addLine(to: CGPoint(x: width, y: 0.0))
+        bezierPath.addLine(to: CGPoint(x: 0.0, y: l3ControlPointView.dg_center(usePresentationLayerIfPossible: animating).y))
         
+        bezierPath.addCurve(to: l1ControlPointView.dg_center(usePresentationLayerIfPossible: animating),
+                            controlPoint1: l3ControlPointView.dg_center(usePresentationLayerIfPossible: animating),
+                            controlPoint2: l2ControlPointView.dg_center(usePresentationLayerIfPossible: animating))
+        
+        bezierPath.addCurve(to: r1ControlPointView.dg_center(usePresentationLayerIfPossible: animating),
+                            controlPoint1: cControlPointView.dg_center(usePresentationLayerIfPossible: animating),
+                            controlPoint2: r1ControlPointView.dg_center(usePresentationLayerIfPossible: animating))
+        
+        bezierPath.addCurve(to: r3ControlPointView.dg_center(usePresentationLayerIfPossible: animating),
+                            controlPoint1: r1ControlPointView.dg_center(usePresentationLayerIfPossible: animating),
+                            controlPoint2: r2ControlPointView.dg_center(usePresentationLayerIfPossible: animating))
+        
+        bezierPath.addLine(to: CGPoint(x: width, y: 0.0))
         bezierPath.close()
         
         return bezierPath.cgPath 
